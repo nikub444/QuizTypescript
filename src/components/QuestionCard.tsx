cb7fa2c1c393bb565d9e84c5bc8f6147ea651a3b
nikub444 <magaszak12@gmail.com>
2021-08-05 22:13:17 +0200

    first step

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answer,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => <div>Question Card</div>;
+
+export default QuestionCard;
